c3c6f8d40c9e8c2eb16e19ab0d8e292db03ba147
regisclaus <regis.claus@gmail.com>
2025-05-19 13:13:21 +0000

    modify phrase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oi Paula, eu te amo e estou estudando programação web, haha!</h1>
+    return <h1>Oi Paula, eu te amo e estou estudando programação web, haha!!!</h1>
 }
 
 export default Home;
